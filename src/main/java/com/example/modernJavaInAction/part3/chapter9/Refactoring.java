8593611fba69a26ed28050d2bfec49a93edde09e
배윤진 <dbswls9925@gmail.com>
2023-08-28 22:39:58 +0900

    chapter 9 정리

--- src/main/java/com/example/modernJavaInAction/part3/chapter9/Refactoring.java
+++ src/main/java/com/example/modernJavaInAction/part3/chapter9/Refactoring.java
@@ -1,5 +1,15 @@
 package com.example.modernJavaInAction.part3.chapter9;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class Refactoring {
     /*
     * 코드 가독성 개선
@@ -24,14 +34,91 @@ public class Refactoring {
     /*템플릿메서드
     * 알고리즘 개요를 제시한 다음에 알고리즘의 일부를 고칠 수 있는 유연함을 제공해야 할 때 사용
     * */
+    public void templateMethodTest(){
+        new OnlineBankingLambda().processCustomer(1336, (Customer c) -> System.out.println("Hello" + c.getName()));
+    }
     /*옵저버
     * 어떤 이벤트가 발생했을 대 객체가 다른 객체 리스트에 자동으로 알림을 보내야 하는 상황에서 사용.
     * */
-    /*의무체인
-    * 한 객체가 어떤 작업을 처리한 다음에 다른 객체로 결과 전달, 다른 객체도 해야할 작업을 전달 하는 식.
-    * */
-    /*팩토리
-    * 인스턴스화 로직을 클라이언트에 노출하지 않고 객체를 만들 때 팩토리 디자인 패턴을 사용.
-    * */
+    public void observerTest(){
+        Feed f = new Feed();
+        f.registerObserver((String tweet) -> {
+            if(tweet != null && tweet.contains("money")){
+                System.out.println("Breaking news in NY!!" + tweet);
+                // 이런식으로 명시적으로 인스턴스화 하지 않고 람다 표현식을 직접 전달해서 실행할 동작을 지정 할 수 있음.
+            }
+        });
+    }
 }
 
+// template method pattern test code
+class OnlineBankingLambda{
+    public void processCustomer(int id, Consumer<Customer> makeCustomerHappy){
+        Customer c = DataBase.getCustomerWithId(id);
+        makeCustomerHappy.accept(c);
+    }
+}
+
+@Getter
+class Customer{
+    private String name;
+}
+
+class DataBase{
+    public static Customer getCustomerWithId(int id){
+        return null;
+    }
+}
+
+interface Observer {
+    void notify(String tweet);
+}
+
+class NYTimes implements Observer {
+
+    @Override
+    public void notify(String tweet) {
+        if(tweet != null && tweet.contains("money")){
+            System.out.println("Breaking news in NY!!" + tweet);
+        }
+    }
+}
+
+class Guardian implements Observer{
+
+    @Override
+    public void notify(String tweet) {
+        if(tweet != null && tweet.contains("queen")){
+            System.out.println("Yet more news from London ..." + tweet);
+        }
+    }
+}
+
+class LeMode implements Observer{
+
+    @Override
+    public void notify(String tweet) {
+        if(tweet != null && tweet.contains("wine")){
+            System.out.println("Today cheese, wine and news!" + tweet);
+        }
+    }
+}
+
+interface Subject{
+    void registerObserver(Observer o);
+    void notifyObservers(String tweet);
+}
+
+class Feed implements Subject{
+    private final List<Observer> observers = new ArrayList<>();
+
+    @Override
+    public void registerObserver(Observer o) {
+        this.observers.add(o);
+    }
+
+    @Override
+    public void notifyObservers(String tweet) {
+        observers.forEach(Observer::notify);
+    }
+}
